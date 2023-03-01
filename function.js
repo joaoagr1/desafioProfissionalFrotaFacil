0bf681f4998ce1e7b90f495565db944731f34db6
Vinicius Vilela <viniciusvilela@pmm.gov.br>
2023-03-01 09:55:00 -0300

    01.03 - redefinição de senha

---
+++ function.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+    var elementBtnEnviar = document.getElementById('enviar') 
+
+    elementBtnEnviar.addEventListener('click', function(event){
+        alert("Senha enviada no e-mail");
+    })
+};
