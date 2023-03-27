fd2c7d8aff94df5d009c02227c511fcf841062d0
joaoRollo <joao.rollo1@gmail.com>
2023-03-27 17:35:41 -0300

    teste limpando o algorito

--- teste.c
+++ teste.c
@@ -1,7 +1,13 @@
+
 #include <stdio.h> 
 #include <stdlib.h>
 #include <string.h>
 
+
+    char cadastroUsuario[30];
+    char cadastroSenha[30];
+    char login[30];
+    char senha[30];
 
 struct pessoa{
     char login[30]; 
@@ -11,20 +17,60 @@ struct pessoa{
 
 int main(){
 
-    char cadastroUsuario[30];
-    char cadastroSenha[30];
+   
 
-    char login[30];
-    char senha[30];
+    
 
+
+    funcaoCadastro();
+    
+
+    strcpy(pessoa.login, cadastroUsuario);
+    strcpy(pessoa.senha, cadastroSenha); 
+    
+
+    funcaoLogin();
+    
+    return 0;
+}
+
+
+
+
+
+
+funcaoCadastro(){
 
     printf("Vamos Cadastrar um usuário\n");
     printf("Insira seu nome de usuário:   \n");
     gets(cadastroUsuario);
     fflush(stdin);
+    
+    printf("Vamos cadastrar uma senha");
+    gets(cadastroSenha);
+    fflush(stdin);
 
-    printf("%s",cadastroUsuario);
-
-
-    return 0;
+    return;
 }
+
+
+
+
+funcaoLogin(){
+    
+    
+    printf("\nlogin:");
+    gets(login);
+    fflush(stdin);
+
+    printf("\nsenha:");
+    gets(senha); 
+    fflush(stdin);
+
+    if ((strcmp(login,pessoa.login)==0) && (strcmp(senha,pessoa.senha)==0)){ 
+        printf("Usuário logado");
+    }else{
+        printf("Login e/ou senha incorretos"); 
+    }
+    return;
+    }
