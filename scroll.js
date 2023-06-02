bca6de7f6bcc4836323a563ac2e1f52803bed3e4
ViniciusMVilela <viniciusvilela10@gmail.com>
2023-06-02 18:53:27 -0300

    hambuerguer menu

--- scroll.js
+++ scroll.js
@@ -1,5 +1,5 @@
 document.addEventListener('scroll', () => {
-    const header = document.querySelector("#navbar");
+    const header = document.querySelector("#scrolled");
     if(window.scrollY > 0) {
         header.classList.add('scrolled');
     } else {
